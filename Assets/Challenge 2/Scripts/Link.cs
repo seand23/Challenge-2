bb90b221db4892acfa58715869fb45a5f7d543ad
seand23 <b00156175@mytudublin.ie>
2023-10-04 15:30:39 +0100

    Initial commit

---
+++ Assets/Challenge 2/Scripts/Link.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour 
+{
+
+	public void OpenGitHub()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://github.com/seand23");
+		#endif
+	}
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+
+}
---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float coolDown = 0.7f;
+    private float lastSpawn = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //will add 1 second evry time it passes using the time feature 
+        lastSpawn += Time.deltaTime;
+        // On spacebar press, send dog , if last spawn is less than cooldown a dog will not spawn
+        if (Input.GetKeyDown(KeyCode.Space) && lastSpawn > coolDown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            //reset spawn cooldown timer
+            lastSpawn = 0.0f;
+        }
+    }
+}
